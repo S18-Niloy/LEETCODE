cc1757c48eee6762e575eb29cc09d84a6ab34701
Soumik Deb Niloy <80791375+S18-Niloy@users.noreply.github.com>
2023-07-06 03:21:25 +0600

    Create Search_2D_matrix.py

---
+++ Search_2D_matrix.py
@@ -0,0 +1,21 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:
+            return False
+
+        m, n = len(matrix), len(matrix[0])
+        left, right = 0, m * n - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            row, col = mid // n, mid % n
+            num = matrix[row][col]
+
+            if num == target:
+                return True
+            elif num < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return False
